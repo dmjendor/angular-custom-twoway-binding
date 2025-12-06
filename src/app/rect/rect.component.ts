7d08a60ed0be90f998c2f11127e7c38de3081253
dmjendor@gmail.com <dmjendor@gmail.com>
2025-12-06 11:12:40 -0500

    Update with newer format for Angular 17.2+

--- src/app/rect/rect.component.ts
+++ src/app/rect/rect.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  input,
+  Input,
+  model,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,12 +16,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-  @Input({ required: true }) size!: { width: string; height: string };
-  @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
+  // @Input({ required: true }) size!: { width: string; height: string };
+  // @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
+  // in Angular 17.2 or later, better way of doing it.
+  size = model.required<{ width: string; height: string }>();
 
   onReset() {
     // ...
     const newSize = { width: '250', height: '250' };
-    this.sizeChange.emit(newSize);
+    // this.sizeChange.emit(newSize);
+    this.size.set(newSize);
   }
 }
